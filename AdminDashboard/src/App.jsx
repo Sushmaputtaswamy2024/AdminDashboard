92c5b8c5ee92b10eb669255dc5270a6fbd008761
Rohithashwa <rohangangodi@gmail.com>
2025-09-13 17:20:50 +0530

    Revert "commit"
    This reverts commit 38f6d6ecd0145fcbdc2b7706afa69c5a8fac9d0b.

--- AdminDashboard/src/App.jsx
+++ AdminDashboard/src/App.jsx
@@ -4,9 +4,6 @@ import Cards from './Components/molecules/Cards'
 import AddFile from './Components/molecules/AddFile'
 import AddPeople from './Components/molecules/AddPeople'
 import Display from './Components/organisms/Display'
-import LoginWrapper from './Components/organisms/LoginWrapper'
-import Navbar from './Components/organisms/Navbar'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
@@ -14,14 +11,7 @@ const App = () => {
       {/* <Home /> */}
       {/* <AddFile /> */}
       {/* <AddPeople /> */}
-      {/* <Display/> */}
-      <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route/>
-      </Routes>
-      </BrowserRouter>
-      <Navbar/>
+      <Display/>
     </div>
   )
 }

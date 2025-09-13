6fde89e095d39281447e66992fd4668ebf18cf90
sushma <sushmap9544@gmail.com>
2025-09-13 14:17:28 +0530

    login css

--- AdminDashboard/src/Components/molecules/Login.jsx
+++ AdminDashboard/src/Components/molecules/Login.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import InputBox from '../atoms/InputBox'
 import Button from '../atoms/Button';
+import './Login.css'
 
 const Login = ({handleLogin}) => {
 
@@ -10,11 +11,16 @@ const Login = ({handleLogin}) => {
     
 
   return (
-    <div>
+    <>
+    <div className='login'>
+      <div className='body'>
+      <h1>Login </h1>
       <InputBox className='username' type='text' placeholder='Username' onChange={(e) => setUsername(e.target.value)} />
       <InputBox className='password' type='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
       <Button className='login-button' label='Login' onClick={() => handleLogin(username, password)} />
+      </div>
     </div>
+    </>
   )
 }
 
